7b84a498b739d969bc1154f57cd6e0844b762b34
smriti <smriti.154@gmail.com>
2017-05-07 01:16:24 +0530

    hh

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,16 +5,16 @@ require 'json'
 class HomeController < ApplicationController
 
 	def index
-		puts "insde"
-          @response1 = github_details('smriti154')
-          puts ">>>>>>>>>>>.", @response1
+		# puts "insde"
+          # @response1 = github_details('smriti154')
+          # puts ">>>>>>>>>>>.", @response1
 		 # @response1 = HTTParty.get('https://github.com/smriti154')
 		  # @response2 = HTTParty.get('https://api.stackexchange.com/2.2/users/22656?order=desc&sort=reputation&site=stackoverflow')
 
 		  # @response3 = HTTParty.get('https://in.pinterest.com/smriti154/')
 
 		  # @response4 = HTTParty.get('https://twitter.com/smriti')
-		  job_posting = 'Senior Software Test Engineer Person should have very good hands on experience on Selenium. Person should have some experience on overall Selenium framework as well so that he is aware how the entire test automation process work on top of Selenium. Experience on Protractor test would be a plus. End to end automation experience. Person shoud be flexible to jump to do manual testing on need basis. Person may also need to guide junior manual QA.'
+		  # job_posting = 'Senior Software Test Engineer Person should have very good hands on experience on Selenium. Person should have some experience on overall Selenium framework as well so that he is aware how the entire test automation process work on top of Selenium. Experience on Protractor test would be a plus. End to end automation experience. Person shoud be flexible to jump to do manual testing on need basis. Person may also need to guide junior manual QA.'
 		  # result = `python public/temp.py`
 		  # puts ".....", result
           # render :text => result
@@ -43,15 +43,10 @@ class HomeController < ApplicationController
     # response = HTTParty.get("https://api.stackexchange.com/2.2/users/:id?order=desc&sort=reputation&site=stackoverflow&id={username}")
 	
 
-    users = HTTParty.get("https://api.github.com/users/#{username}/repos")
+    # users = HTTParty.get("https://api.github.com/users/#{username}/repos")
     # @options = { query: { username: username } }
-      puts "response from github", users.count
-      users.count
-  end
-
-  
-  def users
-    
+      # puts "response from github", users.count
+      # users.count
   end
     
     
